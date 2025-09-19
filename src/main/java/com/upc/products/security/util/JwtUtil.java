c6ce1efa66dce6681195646503c78af585223c0e
pciscflo <pciscflo@upc.edu.pe>
2025-09-19 07:32:35 -0500

    19.09.2025

--- src/main/java/com/upc/products/security/util/JwtUtil.java
+++ src/main/java/com/upc/products/security/util/JwtUtil.java
@@ -47,7 +47,7 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 20)) // 20 min
                 .signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 
